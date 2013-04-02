b6e3f52ad90636c16ebf44d3a171f939a844ce70
axxxon <axxxon.pl@gmail.com>
2013-04-02 20:49:37 +0200

    Some little changes

--- QuickAd/Controllers/HomeController.cs
+++ QuickAd/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuickAd.Models;
 
 namespace QuickAd.Controllers
 {
@@ -10,6 +11,8 @@ namespace QuickAd.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Categories = DBHelper.GetAll<AdvertCategory>();
+            ViewBag.Regions = DBHelper.GetAll<Teritory>();
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
